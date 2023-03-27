c71f928eea1250526e551cc69899779960dd2c6d
FelixCrystal <tbomer@amarokstudios.com>
2023-03-26 21:24:25 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*  -----------------------------------------------------------------
+    Class:
+        Name:           Main
+        Definition:     main.cpp
+        Header:         N/A
+
+    Description:    Acts as the launcher and QA tester.
+    Author:         TBomer, JKantara, ALogan
+    Date:           03/26/2023
+
+    Revision History:
+        Name:           Date:           Description:
+        TBomer          03/26/2023      Initial creation.
+------------------------------------------------------------------- */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello World!";
+}
