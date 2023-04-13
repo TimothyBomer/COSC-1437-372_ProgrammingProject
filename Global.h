2ead023a58fe9f8bf3ada6a5df6f6f90ed90ac61
FelixCrystal <tbomer@amarokstudios.com>
2023-04-12 23:28:26 -0500

    Cleaned Up and Documented Global Functions

--- Global.h
+++ Global.h
@@ -4,13 +4,17 @@
         Definition:     Globals.cpp
         Header:         Globals.h
 
-    Description:    Class to house global functions.
+    Description:    Class to house global functions that can be
+                        re-used anywhere.
+
     Author:         TBomer, JKantara, ALogan
     Date:           03/26/2023
 
     Revision History:
         Name:           Date:           Description:
         TBomer          03/26/2023      Initial creation.
+        TBomer          04/12/2023      Added IsStringNumber()
+                                        Added EraseLineFromFile()
 ------------------------------------------------------------------- */
 #include <iostream>
 #include <string>
@@ -20,44 +24,45 @@ using namespace std;
 #pragma once
 class Global {
     public:
+        // [TBomer] Used to set the current directory. Useful for locating Database files.
         static string CURRENT_DIR;
+        static void SetCurrentDirectory(char** argv) {
+            string directory = argv[0];
+            directory.erase(directory.find_last_of('\\') + 1);
+            unsigned int start_position_to_erase = directory.find("Debug");
+            directory.erase(start_position_to_erase, 6);
+            Global::CURRENT_DIR = directory.append("ProgrammingProject\\");
+        }
 
+        // [TBomer] Checks to see if a file exists.
         static inline bool file_exists(const string& name) {
             struct stat buffer;
             return (stat(name.c_str(), &buffer) == 0);
         }
 
-        static void SetCurrentDirectory(char** argv) {
-            string directory = argv[0];
-            directory.erase(directory.find_last_of('\\') + 1);
-
-            unsigned int start_position_to_erase = directory.find("Debug");
-            directory.erase(start_position_to_erase, 6);
-
-            Global::CURRENT_DIR = directory.append("ProgrammingProject\\");
-        }
-
+        // [TBomer] Checks to see if a string can be cast to a number.
+        //              useful for error handling.
         static bool IsStringNumber(const std::string& s) {
             std::string::const_iterator it = s.begin();
             while (it != s.end() && std::isdigit(*it)) ++it;
             return !s.empty() && it == s.end();
         }
 
+        // [TBomer] Deletes a specific line from a file.
         static void EraseLineFromFile(string path, string eraseLine) {
             string line;
-            ifstream fin;
-
-            fin.open(path);
-            std::ofstream temp;
-            temp.open("temp.txt");
+            ifstream fin(path);
+            ofstream temp("temp.txt");
 
             while (getline(fin, line)) {
-                if (line != eraseLine)
+                if (line != eraseLine) {
                     temp << line << endl;
+                }   
             }
 
             temp.close();
             fin.close();
+
             const char* p = path.c_str();
             remove(p);
             rename("temp.txt", p);
